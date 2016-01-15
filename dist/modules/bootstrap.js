fc1549b8ca53b90b5774c55f2a19eb4c5ea5f9da
Dominik Maier <domenukk@gmail.com>
2016-01-15 02:12:36 +0100

    added build files

--- dist/modules/bootstrap.js
+++ dist/modules/bootstrap.js
@@ -1,8 +1,13 @@
 /// <reference path="../typings/angular2-meteor.d.ts" />
 'use strict';
 var bootstrap_1 = require('angular2/bootstrap');
+var providers_1 = require('./providers');
 function bootstrap(appComponentType, providers) {
     if (providers === void 0) { providers = null; }
+    var bootstrapProviders = providers_1.providers;
+    if (providers) {
+        providers.forEach(function (element) { return bootstrapProviders.push(element); });
+    }
     bootstrap_1.bootstrap(appComponentType, providers);
 }
 exports.bootstrap = bootstrap;
