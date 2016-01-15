3ddfd5164bd1f91194f31b9c495388cea9a6ca0c
Dominik Maier <domenukk@gmail.com>
2016-01-15 02:27:22 +0100

    Added providers to exports

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,4 +1,5 @@
 export * from './modules/mongo_cursor_observer';
 export * from './modules/mongo_cursor_differ';
+export * from './modules/providers';
 export * from './modules/bootstrap';
 export * from './modules/meteor_component';
--- main.ts
+++ main.ts
@@ -2,5 +2,6 @@
 
 export * from './modules/mongo_cursor_observer';
 export * from './modules/mongo_cursor_differ';
+export * from './modules/providers';
 export * from './modules/bootstrap';
 export * from './modules/meteor_component';
